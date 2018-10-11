4295569570a4181a863f79fd80095114193dad53
Winston "WinPlay02" Oberlaender <winplay02_gh@woberlaender.de>
2018-10-11 15:46:30 +0200

    Create Vergleich.java

---
+++ Vergleich.java
@@ -0,0 +1,34 @@
+package palindrom;
+
+import java.util.Scanner;
+
+public class Vergleich {
+
+	public static String input() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Eingabe> ");
+		String s = sc.nextLine();
+		sc.close();
+		return s.toLowerCase();
+	}
+
+	/**
+	 * Absichtlich
+	 * 
+	 * @return Palindrom? true / false
+	 */
+	public static boolean palindrom(String text) {
+		char[] array = text.toCharArray();
+		for (int i = 0; i < array.length / 2; i++) {
+			if (array[i] != array[array.length - 1 - i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String... args) {
+		System.out.println(palindrom(input()) ? "Palindrom! Wow" : "Kein Palindrom! Haha");
+	}
+
+}
