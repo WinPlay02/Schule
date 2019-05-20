30af37d5650c733441855713b185dcf51601b932
malte.rudius <MR1303@web.de>
2019-05-20 11:23:45 +0200

    Kbv-Die Toten Ärzte

--- floyd/LeFloyd_Project/Floyd.java
+++ floyd/LeFloyd_Project/Floyd.java
@@ -2,10 +2,11 @@ package LeFloyd_Project;
 
 /**
  * 
- * @author alexander.greus
- * @author malte.rudius
- * @author winston.oberlaender
- * 
+ * @author		alexander.greus
+ * @author 		malte.rudius
+ * @author 		winston.oberlaender
+ * @version 	pre-alpha 0.1kbvv
+ * @since		20.05.2019 11:11 Uhr
  */
 
 public class Floyd {
