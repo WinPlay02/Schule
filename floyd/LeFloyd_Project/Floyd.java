ed49cca430ccce6a6902521686d4b66679c974ec
WinPlay02 <winplay02_gh@woberlaender.de>
2019-05-20 11:36:37 +0200

    weiteres

--- floyd/LeFloyd_Project/Floyd.java
+++ floyd/LeFloyd_Project/Floyd.java
@@ -2,20 +2,31 @@ package LeFloyd_Project;
 
 /**
  * 
- * @author		alexander.greus
- * @author 		malte.rudius
- * @author 		winston.oberlaender
- * @version 	pre-alpha 0.1kbvv
- * @since		20.05.2019 11:11 Uhr
- * @category	Dat is Käse
+ * @author alexander.greus
+ * @author malte.rudius
+ * @author winston.oberlaender
+ * @version pre-alpha 0.1kbvv
+ * @since 20.05.2019 11:11 Uhr
+ * @category Dat is Käse
  */
 
 public class Floyd {
 
+	public Floyd() {
+
+	}
+
+	int k_anzahldermaximalschritte = 1;
+
 	int[][] adjazenzmatrix = { {} };
 	int[][] distanzmatrix = { {} };
+
+	public void einWegMehr() {
+
+	}
 
 	public static void main(String... args) {
 
 	}
+
 }
