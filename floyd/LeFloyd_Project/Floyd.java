f69fb873464a9405c0cef634bbbc51ffe4041728
alexander.greus <alexgreus51@gmail.com>
2019-05-20 11:15:41 +0200

    :D

--- floyd/LeFloyd_Project/Floyd.java
+++ floyd/LeFloyd_Project/Floyd.java
@@ -1,6 +1,16 @@
 package LeFloyd_Project;
+
+/**
+ * 
+ * @author alexander.greus
+ * @author malte.rudius
+ * @author winston.oberlaender
+ * 
+ */
+
 public class Floyd {
-    public static void main(String... args){
-        
-    }
+
+	public static void main(String... args) {
+
+	}
 }
