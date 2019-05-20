d0fa65fb5c2f412c0cd2fa4d25071652e03cdf25
alexander.greus <alexgreus51@gmail.com>
2019-05-20 11:35:37 +0200

    Kennste schon die Knaten?

---
+++ floyd/LeFloyd_Project/GraphenPortrait.java
@@ -0,0 +1,19 @@
+package LeFloyd_Project;
+
+/**
+ * 
+ * @author		alexander.greus
+ * @author 		malte.rudius
+ * @author 		winston.oberlaender
+ * @version 	pre-alpha 0.1kbvv
+ * @since		20.05.2019 11:11 Uhr
+ * @category	Dat is Käse
+ * @see			Floyd
+ * 				Tolle Klasse :D
+ */
+
+public abstract class GraphenPortrait {
+	
+	KanteKanteKennste[] kante;
+	
+}
